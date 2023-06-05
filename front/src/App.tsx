4b950ed78980db0c2dc7a0de5c3902b9bf0f2359
Yusuke Ueno <yusuke.ueno@techouse.jp>
2023-06-05 10:20:14 +0900

    :sparkles: react-hook-formを使ったフォーム作成

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,35 +1,51 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { ResetButton } from "./ResetButton";
+import { LoginForm } from "./LoginForm";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	// const [login, setLogin] = useState<boolean>(false);
+	const reset = (): void => {
+		setName("");
+	};
+	const [name, setName] = useState<string>("");
+	const updateName = (name: string): void => {
+		setName(name);
+	};
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+	return (
+		<>
+			<div>
+				<a href="https://vitejs.dev" target="_blank">
+					<img src={viteLogo} className="logo" alt="Vite logo" />
+				</a>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<div>
+				{name == "" ? (
+					<LoginForm updateName={updateName} />
+				) : (
+					<h2>Your Name: {name}</h2>
+				)}
+			</div>
+			<ResetButton reset={reset} />
 
-export default App
+			{/* <h1> {user.name}</h1> */}
+
+			{/* <img
+				className="avatar"
+				src={user.imageUrl}
+				style={{ width: user.imageSize, height: user.imageSize }}
+			/> */}
+			<p className="read-the-docs">
+				Click on the Vite and React logos to learn more
+			</p>
+		</>
+	);
+};
+
+export default App;
